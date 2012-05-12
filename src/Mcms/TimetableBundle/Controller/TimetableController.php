0abda4d852db1bf7eccd5616a3011ac45800a960
kulasart <kulasart@yahoo.pl>
2012-05-12 14:29:01 +0200

    Added all edit profile actions.

--- src/Mcms/TimetableBundle/Controller/TimetableController.php
+++ src/Mcms/TimetableBundle/Controller/TimetableController.php
@@ -11,39 +11,13 @@ use Mcms\EmployeeBundle\Entity\Employee;
 class TimetableController extends Controller
 {
     /**
-     * Finds and returns patient timetable entries
+     * List entries
      * 
-     * @param Patient $patient
+     * @param String $roleTheme Role theme name.
+     * @param Patient $patient An Patient entity
+     * @param Employee $employee An Employee entity
      */
-    public function showPatientEntriesAction(Patient $patient)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $entries = $em->getRepository('McmsTimetableBundle:Entry')->findByPatient($patient);
-
-        return $entries;    
-    }
-
-    /**
-     * Finds and returns employee timetable entries
-     * 
-     * @param Employee $employee.
-     */
-    public function showEmployeeEntriesAction(Employee $employee)
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $entries = $em->getRepository('McmsTimetableBundle:Entry')->findByEmployee($employee);
-
-        return $entries;
-    }
-
-    /**
-     * List all entries
-     * @Template("McmsTimetableBundle::showEntries.html.twig")
-     * @param Array $params An array with Patient,Employe
-     */
-    public function listAction(Patient $patient = null, Employee $employee = null)
+    public function listAction(Patient $patient = null, Employee $employee = null,$roleTheme)
     {
         $em = $this->getDoctrine()->getEntityManager();
 
@@ -52,7 +26,30 @@ class TimetableController extends Controller
             'employee' => $employee ? $employee->getId() : null
         ));
 
-        return array('entries' => $entries);
+        return $this->render('McmsTimetableBundle:'.$roleTheme.':list.html.twig', array(
+            'entries' => $entries,
+        ));
+    }
+
+    /**
+     * Finds and display single timetable entry
+     * 
+     * @param String $roleTheme Role theme name.
+     * @param integer $id Time table entry id
+     */
+    public function show($roleTheme, $id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entry = $em->getRepository('McmsTimetableBundle:Entry')->find($id);
+
+        if(!$entry) {
+            throw $this->createNotFoundException('Unable to find timetable entry.');
+        }
+
+        return $this->render('McmsTimetableBundle:'.$roleTheme.':show.html.twig', array(
+            'entry' => $entry
+        ));
     }
 
 
--- src/Mcms/UserBundle/Controller/UserController.php
+++ src/Mcms/UserBundle/Controller/UserController.php
@@ -4,10 +4,19 @@ namespace Mcms\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Mcms\UserBundle\Entity\User;
+use Mcms\UserBundle\Form\Type\UserType;
+use Mcms\UserBundle\Form\Type\ChangePasswordFormType;
+use Mcms\UserBundle\Form\Model\ChangePassword;
+use Mcms\PatientBundle\Form\Type\PatientType;
+use Mcms\EmployeeBundle\Form\Type\EmployeeType;
+
 class UserController extends Controller
 {
     /**
      * List all users
+     * 
+     * @param String $roleTheme Role theme name.
      */
     public function listAction($roleTheme)
     {
@@ -41,10 +50,129 @@ class UserController extends Controller
     }
 
     /**
-     * Display user profile
+     * Displays user profile
+     * 
+     * @param String $roleTheme Role theme name.
      */
-    public function profileAction()
+    public function profileAction($roleTheme)
     {
         return $this->render('McmsUserBundle:'.$roleTheme.':profile.html.twig');
+    }
+
+    /**
+     * Displays a form to edit own profile
+     * 
+     * @param String $roleTheme Role theme name. 
+     */
+    public function editProfileAction($roleTheme)
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $form = $this->createEditProfileForm($user);
+
+        return $this->render('McmsUserBundle:'.$roleTheme.':editProfile.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Updates own profile
+     * 
+     * @param String $roleTheme Role theme name. 
+     */
+    public function updateProfileAction($roleTheme)
+    {
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $reqest = $this->getRequest();
+
+        $form = $this->createEditProfileForm($user);
+        $form->bindRequest($reqest);
+
+        if($form->isValid())
+        {
+            $em = $this->getDoctrine()->getEntityManager();
+
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin.editProfile'));
+        }
+
+        return $this->render('McmsUserBundle:'.$roleTheme.':editProfile.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Displays form to change password
+     * 
+     * @param String $roleTheme Role theme name.
+     */
+    public function changePasswordAction($roleTheme)
+    {
+        $form = $this->createForm(new ChangePasswordFormType(), new ChangePassword());
+
+        return $this->render('McmsUserBundle:'.$roleTheme.':changePassword.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Updates password
+     * 
+     * @param String $roleTheme Role theme name.
+     */
+    public function updatePasswordAction($roleTheme)
+    {
+        $request = $this->getRequest();
+
+        $form = $this->createForm(new ChangePasswordFormType(), new ChangePassword());
+        $form->bindRequest($request);
+
+        if($form->isValid())
+        {
+            $user = $this->get('security.context')->getToken()->getUser();
+            
+            $factory = $this->container->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($user);
+
+            $newPassword = $encoder->encodePassword($form->getData()->getNewPassword(), $user->getSalt());
+
+            $user->setPassword($newPassword);
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin.profile'));
+        }
+
+        return $this->render('McmsUserBundle:'.$roleTheme.':changePassword.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Creates and returns form to edit own profile
+     * 
+     * @param User $user Current logged it user object
+     * @return Form Form to edit own profile.
+     */
+    private function createEditProfileForm(User $user)
+    {
+        $patient=$user->getPatient();
+        $employee = $user->getEmployee();
+
+        $form = $this->createFormBuilder(array('user' => $user, 'patient' => $patient, 'employee' => $employee))
+            ->add('user', new UserType());
+        
+        if($patient)
+            $form->add('patient', new PatientType());
+
+        if($employee)
+            $form->add('employee', new EmployeeType());
+
+        return $form->getForm();
     }
 }
