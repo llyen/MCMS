8d83f7d416babcf94549c189632529e9b57acf59
kulasart <kulasart@yahoo.pl>
2012-06-09 20:51:48 +0200

    Edit profile redirection bug fix. Changed field type in MedicalHistory entry

--- src/Mcms/MedicalHistoryBundle/Form/Type/EntryType.php
+++ src/Mcms/MedicalHistoryBundle/Form/Type/EntryType.php
@@ -10,7 +10,7 @@ class EntryType extends AbstractType
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
-            ->add('content')
+            ->add('content','textarea')
         ;
     }
 
